10e95825b46be51ef81aa5c0c11c3eea51a9e453
Carlos A. <carlosvsk@gmail.com>
2024-12-22 13:52:27 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -80,7 +80,8 @@ stls.height({ pass: { arrayValue: [jsvals.varReader({pass: {
         ],
 
           args,
-        }}/>, (...args:any) => <Elements.Pressable3 pass={{
+        }}/>, 
+        (...args:any) => <Elements.Pressable3 pass={{
           elementProperties: [{}],
 
           styles: [
@@ -139,7 +140,25 @@ stls.height({ pass: { arrayValue: [jsvals.varReader({pass: {
         }})],
 
           args,
-        }}/>],
+        }}/>, 
+
+    (...args:any) => <Elements.ImageBox pass={{
+      elementsProperties:[{}],
+
+      styles:[
+              {
+                height: "50px",
+                width: "80px",
+                backgroundColor: "#FDFDFD",
+                borderWidth: 2,
+                borderColor: "#EEE"
+              }
+              ],
+
+      URIvariablePath:["https://images.unsplash.com/photo-1488831861984-179da3647265?q=80&w=1228&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+
+      args,
+    }}/>],
 
           startFunctions:[async (...args) =>
         functions.firebase.fireInit({ args, pass:{
